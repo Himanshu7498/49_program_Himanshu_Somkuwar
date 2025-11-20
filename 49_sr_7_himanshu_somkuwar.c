935dc942cae83cd5a22fff4d2fc86114bbdccc04
Himanshu7498 <himanshusomkuwar28wk@gmail.com>
2025-11-20 22:02:12 +0530

    Add files via upload

---
+++ 49_sr_7_himanshu_somkuwar.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char c) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+        exit(1);
+    }
+    stack[++top] = c;
+}
+
+char pop() {
+    if (top == -1) return '\0';
+    return stack[top--];
+}
+
+char peek() {
+    if (top == -1) return '\0';
+    return stack[top];
+}
+
+int precedence(char op) {
+    if (op == '^') return 3;
+    if (op == '*' || op == '/' || op == '%') return 2;
+    if (op == '+' || op == '-') return 1;
+    return 0;
+}
+
+int isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '^');
+}
+
+void infixToPostfix(char *infix, char *postfix) {
+    int i, k = 0;
+    for (i = 0; i < strlen(infix); i++) {
+        char c = infix[i];
+        if (isalnum(c)) {
+            postfix[k++] = c;
+        } else if (c == '(') {
+            push(c);
+        } else if (c == ')') {
+            while (top != -1 && peek() != '(') {
+                postfix[k++] = pop();
+            }
+            pop();
+        } else if (isOperator(c)) {
+            while (top != -1 && precedence(peek()) >= precedence(c)) {
+                postfix[k++] = pop();
+            }
+            push(c);
+        }
+    }
+    while (top != -1) {
+        postfix[k++] = pop();
+    }
+    postfix[k] = '\0';
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    printf("Enter infix expression: ");
+    scanf("%s", infix);
+
+    infixToPostfix(infix, postfix);
+
+    printf("Postfix expression: %s\n", postfix);
+
+    return 0;
+}
